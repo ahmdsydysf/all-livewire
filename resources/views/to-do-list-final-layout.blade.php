0b6bbbc66339c78e4c06979751f6a0b2af53a133
Ah-Sy-Yf <a.sayed.xc@gmail.com>
2024-02-27 17:55:10 +0200

    u1

--- resources/views/to-do-list-final-layout.blade.php
+++ resources/views/to-do-list-final-layout.blade.php
@@ -12,7 +12,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <!-- Styles -->
         <style>
-            .gradient-custom-2 {
+                        .gradient-custom-2 {
                 /* fallback for old browsers */
                 background: #7e40f6;
 
@@ -29,16 +29,16 @@
                     rgba(126, 64, 246, 1),
                     rgba(80, 139, 252, 1)
                 );
-            }
+                }
 
-            .mask-custom {
+                .mask-custom {
                 background: rgba(24, 24, 16, 0.2);
                 border-radius: 2em;
                 backdrop-filter: blur(25px);
                 border: 2px solid rgba(255, 255, 255, 0.05);
                 background-clip: padding-box;
                 box-shadow: 10px 10px 10px rgba(46, 54, 68, 0.03);
-            }
+                }
         </style>
     </head>
     <body class="antialiased">
